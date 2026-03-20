e2f449635617917e98d96588f9f95cd58aaa2d7b
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-20 22:18:16 +0530

    Day1: constant variable example in constant.cpp
    Demonstrates the use of constant variables in C++.

---
+++ C++/Day1/constant.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //int a=5;
+    //cout<<"The value of a was "<<a<<endl;
+    //a=7;
+    //cout<<"The value of a is "<<a<<endl;
+
+
+    // Constant Variable ******
+
+    const int a=98;
+    cout<<"The value of a was "<<a<<endl;
+    //a=8;---> i will get error because 'a' is a constant
+    //cout<<"The value of a is "<<a<<endl;
+    // this will not happen because 'a' no. will not same, in const the value is fixed it never put in any variable 
+
+    return 0;
+
+}
