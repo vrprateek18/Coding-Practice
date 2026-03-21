133396dba8534c0b0acfd60affd0d9adac350183
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-21 21:42:28 +0530

    Day 2:  reference_variable.cpp to demonstrate reference usage

---
+++ C++/Day 2/reference_variable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+    // REFERENCE VARIABLE********
+
+int main (){
+
+    float x=654;
+    float & y=x;
+
+    cout<<x<<endl;
+    cout<<y<<endl;
+
+    return 0;
+}
