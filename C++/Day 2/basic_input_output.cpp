9511acb753497ccdf7848fdd92cea274c61ab23e
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-21 21:32:40 +0530

    Day 2: Implement basic input and output for sum calculation
    Fix output statement to correctly sum num1 and num2.

---
+++ C++/Day 2/basic_input_output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int num1 , num2;
+
+    cout<<"Enter the value of num1:\n"; // '<<' this is insertion operator
+    cin>>num1; // '>>' this is extraction operator
+    
+    
+    cout<<"Enter the value of num2:\n"; // '<<' this is insertion operator
+    cin>>num2; // '>>' this is extraction operator
+
+    cout<<"The sum is " << num1+num2;
+
+    return 0;
+}
