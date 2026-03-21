c017d9bb937c0c992ea745b04fe1b5e9b0256842
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-21 21:46:16 +0530

    Day 2: typecasting example in typecasting.cpp

---
+++ C++/Day 2/typecasting.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    int a=46;
+    float b=46.5;
+
+    cout<<"The value of a is "<<(float)a<<endl;
+    cout<<"The value of a is "<<float(a)<<endl;
+
+    cout<<"The value of b is "<<(int)b<<endl;
+    cout<<"The value of b is "<<int(b)<<endl;
+    int c=int(b);
+
+    cout<<"The expression is "<<a+b<<endl;
+    cout<<"The expression is "<<a+(int)b<<endl;
+    cout<<"The expression is "<<a+int(c)<<endl;
+
+    return 0;
+    
+}
