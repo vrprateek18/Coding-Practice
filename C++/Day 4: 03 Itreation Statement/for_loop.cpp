0be233e87e9a8aef1ff007c0e4fc061309cb7508
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-24 18:42:44 +0530

    Day 4: for loop example in for_loop.cpp
    Implement a for loop to print numbers from 1 to 39.

---
+++ C++/Day 4: 03 Itreation Statement/for_loop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    /*
+    Loops in C++
+    There are three types of loops in C++
+       1. For Loop
+       2. While Loop
+       3. Do While loop
+    */
+    /*
+    For Loop in C++
+    Syntax for "For Loop" in C++
+    for(initialization; condition; updation) 
+    {
+            loop body(C++ code);
+    }
+    */
+
+    for (int i=1;i<40;i++)
+    {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
