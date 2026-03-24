04b8027f9125c7d5f5a121980c1f71c34c41ad2b
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-24 18:41:20 +0530

    Day 3:  do-while loop example in do_while_loop.cpp
    Implement a do-while loop to print numbers from 1 to 40.

---
+++ C++/Day 4: 03 Itreation Statement/do_while_loop.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    /* Do While loop in C++
+    Syntax:
+    do
+    {
+        C++ (Statement);
+    }while (condition);
+    */
+    // printing 1 to 40 no. in do while loop
+
+    int i=1;
+    do 
+    {
+        cout<<i<<endl;
+        i++;
+    }while (i<=40);
+
+    return 0;
+}
+
+
+/* 
+while & do while loop mein difference yeh ho ta hai 
+do while mein wo aak baar chalega hee 
+aur 
+while loop mein vo pura print hoga usme true & false ki condition apply nahi hogi
+*/
