dcb9b7921fff4355c6720d815503f2d6fc371efe
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-24 18:44:07 +0530

    Day 4: while loop to print numbers from 1 to 40
    Implement a while loop to print numbers from 1 to 40.

---
+++ C++/Day 4: 03 Itreation Statement/while_loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    /*
+    While Loop in C++
+    Syntax:
+    while(condition):
+    {
+        C++ statements;
+    }
+    */
+
+    // Printing 1 to 40 using while loop
+
+    int i=1;
+    while(i<=40){
+        cout<<i<<endl;
+        i++;
+    }
+
+    return 0;
+}
