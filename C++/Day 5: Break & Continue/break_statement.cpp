65b92d9d6139891a90c7e397018dbc518132844e
PRATEEK VERMA <vrprateek.18@gmail.com>
2026-03-25 21:59:55 +0530

    Day 5: Implement break statement in for loop example
    Uncommented the for loop and added output for values.

---
+++ C++/Day 5: Break & Continue/break_statement.cpp
@@ -0,0 +1,32 @@
+#include<iostream>;
+using namespace std;
+    
+int main(){
+    
+    // for (int i = 0; i < 40; i++)
+    // {
+    //     /* code */
+    //     cout<<i<<endl;
+
+    //     if(i==2)
+    //     {
+    //         break;
+    //     }
+    // }
+    // This is different thing
+
+    for (int i = 0; i < 40; i++)
+    {
+        /* code */
+        
+        if(i==2)
+        {
+            break;
+        }
+        cout<<i<<endl;
+    }
+
+    // This is different thing
+    
+    return 0;
+}
